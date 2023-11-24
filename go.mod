aba7c47b8c70670b3be6c7dd7fde134cbe868ace
Christian Simon <simon@swine.de>
2023-11-24 20:08:46 +0000

    Update to make zfs snapshot metrics work

--- go.mod
+++ go.mod
@@ -4,27 +4,31 @@ go 1.20
 
 require (
 	github.com/davecgh/go-spew v1.1.1
-	github.com/prometheus/client_golang v1.12.2
+	github.com/prometheus/client_golang v1.17.0
 	github.com/rs/zerolog v1.31.0
-	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a
+	github.com/stretchr/testify v1.4.0
+	golang.org/x/sync v0.3.0
 )
 
 require (
+	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
 
 require (
 	git.dolansoft.org/lorenz/go-zfs v0.0.0-20211205151927-3df88f338885
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
-	github.com/prometheus/client_model v0.2.0 // indirect
-	github.com/prometheus/common v0.32.1 // indirect
-	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/prometheus/client_model v0.4.1-0.20230718164431-9a2bf3000d16 // indirect
+	github.com/prometheus/common v0.44.0 // indirect
+	github.com/prometheus/procfs v0.11.1 // indirect
 	golang.org/x/sys v0.12.0 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
 
 replace git.dolansoft.org/lorenz/go-zfs => github.com/lorenz/go-zfs v0.0.0-20230225101216-f5e946970e2d
--- main.go
+++ main.go
@@ -8,64 +8,192 @@ import (
 	"os"
 	"os/signal"
 	"strings"
+	"sync"
 	"time"
 
 	"git.dolansoft.org/lorenz/go-zfs/ioctl"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/collectors"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"golang.org/x/sync/errgroup"
 )
 
-type datasetSnapshotState struct {
-	snapshots map[string]time.Time
+type snapshot struct {
+	name string
+	ts   time.Time
+	used uint64
 }
 
 type zfsSnapshotState struct {
-	datasets map[string]datasetSnapshotState
+	lck sync.Mutex
+
+	snapshotFilter func(string) bool
+
+	// snapshots slice is ordered by timestamp
+	datasets map[string][]snapshot
+
+	metricCount        *prometheus.GaugeVec
+	metricLastUnixtime *prometheus.GaugeVec
+	metricDiskUsed     *prometheus.GaugeVec
+}
+
+func newZfsSnapshotState(reg prometheus.Registerer) *zfsSnapshotState {
+	return &zfsSnapshotState{
+		datasets: make(map[string][]snapshot),
+
+		metricCount: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "zfs",
+			Subsystem: "snapshot",
+			Name:      "count",
+			Help:      "Count of existing ZFS snapshots.",
+		}, []string{"dataset"}),
+		metricDiskUsed: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "zfs",
+			Subsystem: "snapshot",
+			Name:      "disk_used",
+			Help:      "Disk space used by all snapshots.",
+		}, []string{"dataset"}),
+		metricLastUnixtime: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: "zfs",
+			Subsystem: "snapshot",
+			Name:      "last_unixtime",
+			Help:      "Time of last ZFS snapshot",
+		}, []string{"dataset"}),
+	}
+}
+
+func (z *zfsSnapshotState) Describe(ch chan<- *prometheus.Desc) {
+	z.lck.Lock()
+	defer z.lck.Unlock()
+
+	z.metricCount.Describe(ch)
+	z.metricDiskUsed.Describe(ch)
+	z.metricLastUnixtime.Describe(ch)
+}
+
+func (z *zfsSnapshotState) Collect(ch chan<- prometheus.Metric) {
+	z.lck.Lock()
+	defer z.lck.Unlock()
+
+	z.metricCount.Reset()
+	z.metricDiskUsed.Reset()
+	z.metricLastUnixtime.Reset()
+
+	var (
+		used uint64
+		last time.Time
+	)
+
+	for dataset, snapshots := range z.datasets {
+		z.metricCount.WithLabelValues(dataset).Set(float64(len(snapshots)))
+		used = 0
+		last = time.Time{}
+		for _, snap := range snapshots {
+			used += snap.used
+			last = snap.ts
+		}
+		z.metricDiskUsed.WithLabelValues(dataset).Set(float64(used))
+		z.metricLastUnixtime.WithLabelValues(dataset).Set(float64(last.Unix()))
+	}
+
+	z.metricCount.Collect(ch)
+	z.metricDiskUsed.Collect(ch)
+	z.metricLastUnixtime.Collect(ch)
+}
+
+func (z *zfsSnapshotState) add(datasetName string, snapshotName string, size uint64, time time.Time) {
+	z.lck.Lock()
+	defer z.lck.Unlock()
+
+	snap := snapshot{
+		name: snapshotName,
+		ts:   time,
+		used: size,
+	}
+
+	// append to ordered slice, if not existing
+	snapshots, ok := z.datasets[datasetName]
+	if !ok {
+		z.datasets[datasetName] = []snapshot{snap}
+		return
+	}
+
+	// check if already there, and where it would fit
+	idx := 0
+	for _, s := range snapshots {
+		if s.name == snapshotName {
+			return
+		}
+		if s.ts.After(time) {
+			break
+		}
+		idx++
+	}
+
+	// insert at idx
+	z.datasets[datasetName] = append(z.datasets[datasetName], snapshot{})
+	copy(z.datasets[datasetName][idx+1:], z.datasets[datasetName][idx:])
+	z.datasets[datasetName][idx] = snap
+
 }
 
 var (
 	nodePath = "/dev/zfs"
 	err      error
 
-	logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339Nano}).With().Timestamp().Logger().Level(zerolog.DebugLevel)
+	logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339Nano}).With().Timestamp().Logger().Level(zerolog.InfoLevel)
 )
 
-var (
-	snapshotCount = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "zfs",
-		Subsystem: "snapshot",
-		Name:      "count",
-		Help:      "Snapshots per dataset",
-	}, []string{"dataset"})
-	lastSnapshot = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "zfs",
-		Subsystem: "snapshot",
-		Name:      "last_created_unix",
-		Help:      "Unixtime of last snapshot created",
-	}, []string{"dataset"})
-)
+var ()
+
+func getUsedBytes(prop ioctl.DatasetPropsWithSource) (uint64, bool) {
+	v, ok := prop["used"]
+	if !ok {
+		return 0, false
+	}
+
+	vuint, ok := v.Value.(uint64)
+	if !ok {
+		return 0, false
+	}
+
+	return vuint, true
+}
+
+func getCreationTime(prop ioctl.DatasetPropsWithSource) (time.Time, bool) {
+	v, ok := prop["creation"]
+	if !ok {
+		return time.Time{}, false
+	}
+
+	vint, ok := v.Value.(uint64)
+	if !ok {
+		return time.Time{}, false
+	}
+
+	return time.Unix(int64(vint), 0), true
+}
 
 func run(ctx context.Context) func() error {
 	return func() error {
+		snapshots := newZfsSnapshotState(prometheus.DefaultRegisterer)
+
+		prometheus.MustRegister(snapshots)
+
 		ioctl.Init("")
 
 		var (
-			i               uint64
-			name            string
-			snapshotName    string
-			err             error
-			props           ioctl.DatasetPropsWithSource
-			j               uint64
-			dsSnapshotCount uint64
-			dsLastSnapshot  time.Time
-			stats           ioctl.DMUObjectSetStats
-			snapshotProps   = make(ioctl.DatasetPropsWithSource)
+			i             uint64
+			name          string
+			snapshotName  string
+			err           error
+			props         ioctl.DatasetPropsWithSource
+			j             uint64
+			stats         ioctl.DMUObjectSetStats
+			snapshotProps = make(ioctl.DatasetPropsWithSource)
 		)
 
 		// get dataset list
@@ -96,8 +224,6 @@ func run(ctx context.Context) func() error {
 				datasets = append(datasets, name)
 
 				j = 0
-				dsSnapshotCount = 0
-				dsLastSnapshot = time.Time{}
 
 				for {
 					snapshotName, j, stats, err = ioctl.SnapshotListNext(name, j, snapshotProps)
@@ -109,23 +235,22 @@ func run(ctx context.Context) func() error {
 					}
 
 					snapshotName = strings.Split(snapshotName, "@")[1]
-					ts, err := time.Parse(*snapshotTemplate, snapshotName)
-					if err != nil {
-						logger.Warn().Msgf("failed to parse timestamp from snapshot name %s: %v", snapshotName, err)
+
+					logger.Debug().Msgf("snapshot %s (%d) props=%+v", snapshotName, j, snapshotProps)
+
+					used, ok := getUsedBytes(snapshotProps)
+					if !ok {
+						continue
+					}
+					creation, ok := getCreationTime(snapshotProps)
+					if !ok {
 						continue
 					}
 
-					if ts.After(dsLastSnapshot) {
-						dsLastSnapshot = ts
-					}
+					snapshots.add(name, snapshotName, used, creation)
 
-					logger.Debug().Msgf("snapshot %s (%d) props=%+v", snapshotName, j, snapshotProps)
 					logger.Trace().Msgf("snapshot stats=%s", spew.Sdump(stats))
-					dsSnapshotCount++
 				}
-				snapshotCount.WithLabelValues(name).Set(float64(dsSnapshotCount))
-				lastSnapshot.WithLabelValues(name).Set(float64(dsLastSnapshot.Unix()))
-
 			}
 		}
 		return nil
@@ -195,8 +320,8 @@ func watchEvents() error {
 }
 
 var (
-	listenAddr       = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
-	snapshotTemplate = flag.String("snapshot-template", "zrepl_20060102_150405_000", "The template for reading timestamps from snapshot names")
+	listenAddr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
+	logLevel   = flag.String("log-level", "info", "Log level")
 )
 
 func main() {
@@ -206,6 +331,13 @@ func main() {
 	prometheus.MustRegister(collectors.NewBuildInfoCollector())
 
 	flag.Parse()
+
+	// setting log level appropriately
+	lvl, err := zerolog.ParseLevel(*logLevel)
+	if err != nil {
+		logger.Fatal().Msgf("invalid log level: %v", err)
+	}
+	logger = logger.Level(lvl)
 
 	g, ctx := errgroup.WithContext(ctx)
 
